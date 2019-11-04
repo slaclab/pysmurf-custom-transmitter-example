5c59551222a0f45360d823269da78b9fdd339513
cryo <cryo@smurf-srv11>
2019-11-04 11:03:04 -0800

    Add a python wrapper for the transmitter example

---
+++ python/mymodule/transmitters/_MyTransmitter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# Title      : PySMuRF Data Base Transmitter
+#-----------------------------------------------------------------------------
+# File       : __init__.py
+# Created    : 2019-09-30
+#-----------------------------------------------------------------------------
+# Description:
+#    SMuRF Data MyTransmitter Python Package
+#-----------------------------------------------------------------------------
+# This file is part of the smurf software platform. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the smurf software platform, including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+
+import pyrogue
+import smurf
+import MyModule
+
+class MyTransmitter(pyrogue.Device):
+    """
+    SMuRF Data MyTransmitter Python Wrapper.
+    """
+    def __init__(self, name, **kwargs):
+        pyrogue.Device.__init__(self, name=name, description='SMuRF Data CustomTransmitter', **kwargs)
+        self._transmitter = MyModule.MyTransmitter()
+
+    # Method called by streamConnect, streamTap and streamConnectBiDir to access slave
+    def _getStreamSlave(self):
+        return self._transmitter
+
