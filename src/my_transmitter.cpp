3a06f56e15e4abadf1dd64f25dc1b32c56785052
cryo <cryo@smurf-srv11>
2019-11-04 19:30:32 +0000

    Add debug flag, settable from python

--- src/my_transmitter.cpp
+++ src/my_transmitter.cpp
@@ -4,34 +4,58 @@
 namespace bp = boost::python;
 namespace sct = smurf::core::transmitters;
 
+// Custom transmitter class
 class MyTransmitter : public sct::BaseTransmitter
 {
 public:
+    // Custom class constructor and destrcutor
+    MyTransmitter() : sct::BaseTransmitter(), debug(false) {};
+    ~MyTransmitter() {};
+
+    // This is the virtal method defined in 'BaseTransmitter' which is call whenever a
+    // new SMuRF packet is ready.
     void transmit(SmurfPacketROPtr sp)
     {
-      std::cout << "=====================================" << std::endl;
-      std::cout << "Packet received" << std::endl;
-      std::cout << "=====================================" << std::endl;
-      std::cout << std::endl;
-      std::cout << "-----------------------" << std::endl;
-      std::cout << " HEADER:" << std::endl;
-      std::cout << "-----------------------" << std::endl;
-      std::cout << "-----------------------" << std::endl;
-      std::cout << " DATA (first 20 points):" << std::endl;
-      std::cout << "-----------------------" << std::endl;
-      std::cout << "-----------------------" << std::endl;
-      std::cout << std::endl;
-      std::cout << "=====================================" << std::endl;
+	// If the debug flag is enabled, print part of the SMuRF Packet
+        if (debug)
+        {
+            std::cout << "=====================================" << std::endl;
+            std::cout << "Packet received" << std::endl;
+            std::cout << "=====================================" << std::endl;
+            std::cout << std::endl;
+            std::cout << "-----------------------" << std::endl;
+            std::cout << " HEADER:" << std::endl;
+            std::cout << "-----------------------" << std::endl;
+            std::cout << "-----------------------" << std::endl;
+            std::cout << " DATA (first 20 points):" << std::endl;
+            std::cout << "-----------------------" << std::endl;
+            std::cout << "-----------------------" << std::endl;
+            std::cout << std::endl;
+            std::cout << "=====================================" << std::endl;
+        }
     };
 
-    static void setup_python()
-      {
-        bp::class_<MyTransmitter, std::shared_ptr<MyTransmitter>, bp::bases<ris::Slave>, boost::noncopyable >("MyTransmitter",bp::init<>())
-        ;
+    // Set/Get the debug flag
+    void       setDebug(bool d) { debug = d;    };
+    const bool getDebug()       { return debug; };
 
+    static void setup_python()
+    {
+        bp::class_< MyTransmitter, 
+	            std::shared_ptr<MyTransmitter>, 
+		    bp::bases<ris::Slave>, 
+		    boost::noncopyable >("MyTransmitter",bp::init<>())
+            .def("setDebug", &MyTransmitter::setDebug)
+            .def("getDebug", &MyTransmitter::getDebug)
+        ;
         bp::implicitly_convertible<std::shared_ptr<MyTransmitter>, ris::SlavePtr>();
-      };
+    };
+
+private:
+    bool debug = false; // Debug flag
+
 };
+
 
 BOOST_PYTHON_MODULE(MyModule)
 {
