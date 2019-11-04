e126271b48417b6f1ca0b2be610906c67a2e1726
cryo <cryo@smurf-srv11>
2019-11-04 09:40:24 -0800

    Add custom transmitter class example

---
+++ src/my_transmitter.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "smurf/core/transmitters/BaseTransmitter.h"
+
+namespace bp = boost::python;
+namespace sct = smurf::core::transmitters;
+
+class MyTransmitter : public sct::BaseTransmitter
+{
+public:
+    void transmit(SmurfPacketROPtr sp)
+    {
+      std::cout << "=====================================" << std::endl;
+      std::cout << "Packet received" << std::endl;
+      std::cout << "=====================================" << std::endl;
+      std::cout << std::endl;
+      std::cout << "-----------------------" << std::endl;
+      std::cout << " HEADER:" << std::endl;
+      std::cout << "-----------------------" << std::endl;
+      std::cout << "-----------------------" << std::endl;
+      std::cout << " DATA (first 20 points):" << std::endl;
+      std::cout << "-----------------------" << std::endl;
+      std::cout << "-----------------------" << std::endl;
+      std::cout << std::endl;
+      std::cout << "=====================================" << std::endl;
+    };
+
+    static void setup_python()
+      {
+        bp::class_<MyTransmitter, std::shared_ptr<MyTransmitter>, bp::bases<ris::Slave>, boost::noncopyable >("MyTransmitter",bp::init<>())
+        ;
+
+        bp::implicitly_convertible<std::shared_ptr<MyTransmitter>, ris::SlavePtr>();
+      };
+};
+
+BOOST_PYTHON_MODULE(MyModule)
+{
+  PyEval_InitThreads();
+
+  try
+  {
+    MyTransmitter::setup_python();
+  }
+  catch (...)
+  {
+    printf("Failed to load my processor module. import rogue first\n");
+  }
+
+  printf("Loaded my processor module\n");
+};
