86029ccd1ec53b253415db25dd9096b2819e9870
cryo <cryo@smurf-srv11>
2019-11-04 20:08:51 +0000

    Print some header and data info in debug mode

--- src/my_transmitter.cpp
+++ src/my_transmitter.cpp
@@ -19,18 +19,37 @@ public:
 	// If the debug flag is enabled, print part of the SMuRF Packet
         if (debug)
         {
+	    std::size_t numCh {sp->getHeader()->getNumberChannels()};
             std::cout << "=====================================" << std::endl;
             std::cout << "Packet received" << std::endl;
             std::cout << "=====================================" << std::endl;
             std::cout << std::endl;
+
             std::cout << "-----------------------" << std::endl;
             std::cout << " HEADER:" << std::endl;
             std::cout << "-----------------------" << std::endl;
+	    std::cout << "Version            = " << unsigned(sp->getHeader()->getVersion()) << std::endl;
+	    std::cout << "Crate ID           = " << unsigned(sp->getHeader()->getCrateID()) << std::endl;
+            std::cout << "Slot number        = " << unsigned(sp->getHeader()->getSlotNumber()) << std::endl;
+            std::cout << "Number of channels = " << unsigned(numCh) << std::endl;
+            std::cout << "Unix time          = " << unsigned(sp->getHeader()->getUnixTime()) << std::endl;
+            std::cout << "Frame counter      = " << unsigned(sp->getHeader()->getFrameCounter()) << std::endl;
+            std::cout << std::endl;
+
             std::cout << "-----------------------" << std::endl;
-            std::cout << " DATA (first 20 points):" << std::endl;
+            std::cout << " DATA (up to the first 20 points):" << std::endl;
             std::cout << "-----------------------" << std::endl;
+
+	    std::size_t n{20};
+	    if (numCh < n)
+		    n = numCh;
+
+	    for (std::size_t i(0); i < n; ++i)
+                std::cout << "Data[" << i << "] = " << sp->getData(i) << std::endl;
+
             std::cout << "-----------------------" << std::endl;
             std::cout << std::endl;
+
             std::cout << "=====================================" << std::endl;
         }
     };
