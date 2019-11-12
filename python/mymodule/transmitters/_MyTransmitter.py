1902f629d7f5c66e839cb686abd39557171212fe
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-11-11 23:15:06 -0800

    Initialize the base class

--- python/mymodule/transmitters/_MyTransmitter.py
+++ python/mymodule/transmitters/_MyTransmitter.py
@@ -28,7 +28,7 @@ class MyTransmitter(BaseTransmitter):
     SMuRF Data MyTransmitter Python Wrapper.
     """
     def __init__(self, name, **kwargs):
-        pyrogue.Device.__init__(self, name=name, description='SMuRF Data CustomTransmitter', **kwargs)
+        BaseTransmitter.__init__(self, name=name, description='SMuRF Data CustomTransmitter', **kwargs)
         self._transmitter = MyModule.MyTransmitter()
 
         # Add "Disable" variable
